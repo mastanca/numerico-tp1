0b0ca19409f18e734044f56bc7c33c750f2720d1
Martin Stancanelli <mastanca14@gmail.com>
2016-04-16 17:23:03 -0300

    Use size of matrix

--- tp_terminado.m
+++ tp_terminado.m
@@ -1,15 +1,17 @@
+SIZE_OF_MATRIX = 48;
+
 # Loading lower diagonal matrix
-A = zeros(48,48);
-A(9:49:450) = -1; 
+A = zeros(SIZE_OF_MATRIX,SIZE_OF_MATRIX);
+A(9:SIZE_OF_MATRIX + 1:SIZE_OF_MATRIX*9+18) = -1; 
 # Use 2 (4/2) to avoid the sum of the diagonal when transposing
-A(1:49:end) = 2; 
-A(2:49:296) = -1;
-A(48*8+10:49:48*14+16) = -1;
-A(14*48+19:49:29*48+34) = -1;
-A(16*48+18:49*2:48*30+32) = -1;
-A(48*30+39:49:48*40+47) = -1;
-A(48*32+34:49:48*38+40) = -1;
-A(48*40+42:49:48*47) = -1;
+A(1:SIZE_OF_MATRIX + 1:end) = 2; 
+A(2:SIZE_OF_MATRIX + 1:SIZE_OF_MATRIX*6+8) = -1;
+A(SIZE_OF_MATRIX*8+10:SIZE_OF_MATRIX + 1:SIZE_OF_MATRIX*14+16) = -1;
+A(14*SIZE_OF_MATRIX+19:SIZE_OF_MATRIX + 1:29*SIZE_OF_MATRIX+34) = -1;
+A(16*SIZE_OF_MATRIX+18:SIZE_OF_MATRIX + 1*2:SIZE_OF_MATRIX*30+32) = -1;
+A(SIZE_OF_MATRIX*30+39:SIZE_OF_MATRIX + 1:SIZE_OF_MATRIX*40+47) = -1;
+A(SIZE_OF_MATRIX*32+34:SIZE_OF_MATRIX + 1:SIZE_OF_MATRIX*38+40) = -1;
+A(SIZE_OF_MATRIX*40+42:SIZE_OF_MATRIX + 1:SIZE_OF_MATRIX*47) = -1;
 
 # Transpose and sum to get full matrix
 B = transpose(A);
